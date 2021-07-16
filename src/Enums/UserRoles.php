336eb566309aa311af43caa608928583117dc250
JyToSTT <jean.tostivint@gmail.com>
2021-07-16 15:17:53 +0200

    > Added enums

---
+++ src/Enums/UserRoles.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Enums;
+
+abstract class UserRoles
+{
+    const ROLE_ADMIN = 'ROLE_ADMIN';
+    const ROLE_BACK_OFFICE = 'ROLE_BACK_OFFICE';
+    const ROLE_USER = 'ROLE_USER';
+}
