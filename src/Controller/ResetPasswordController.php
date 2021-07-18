d553c05dcc106e898392ddfde4ee0e47a29aa21a
RomainDreidemy <dreidemyromain@gmail.com>
2021-07-18 15:27:37 +0200

    Password reset

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -2,23 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Services\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ResetPasswordController extends AbstractController
 {
-    public function __invoke(Request $request, UserService $userService): JsonResponse
-    {
-        $content = json_decode($request->getContent(), true);
 
-        if(!$userService->resetPassword($this->getUser(), $content['password'])){
+    public function __construct(private UserService $userService){}
+
+    #[Route('/api/users/password-token', name: 'user_password_token' ,methods: ['POST'])]
+    public function generateToken(Request $request): JsonResponse
+    {
+        $email = json_decode($request->getContent(), true)['email'] ?? null;
+
+        if(is_null($email)){
             return $this->json([], 400);
         }
 
-        return $this->json(
-            ['password' => $content['password']]
-        );
+        if(!$token = $this->userService->generateToken($email)){
+            return $this->json([], 400);
+        }
+
+        return $this->json(['token' => $token]);
+    }
+
+    #[Route('/api/users/password-refresh', name: 'user_password_refresh' ,methods: ['POST'] )]
+    public function refreshPassword(Request $request): JsonResponse
+    {
+        $content = json_decode($request->getContent(), true);
+
+        $token = $content['token'] ?? null;
+        $password = $content['password'] ?? null;
+
+        if(is_null($token) || is_null($password)){
+            return $this->json(['message' => 'Token ou password n\'est pas présent.'], 400);
+        }
+
+        if(!$this->userService->resetPassword($token, $password)){
+            return $this->json(['message' => 'L\'utilisateur n\'a pas pu être retrouvé.'], 400);
+        }
+
+        return $this->json(['message' => 'Le mot de passe est modifié.']);
     }
 }
