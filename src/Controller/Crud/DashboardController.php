9ecdc19db66e56e13967f0ca1a5b319e69145170
JyToSTT <jean.tostivint@gmail.com>
2021-07-22 10:44:15 +0200

    > Adding selector

--- src/Controller/Crud/DashboardController.php
+++ src/Controller/Crud/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Crud;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/', name: 'dashboard_index')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+            'users' => $userRepository->getSchoolUsers(),
         ]);
     }
 }
