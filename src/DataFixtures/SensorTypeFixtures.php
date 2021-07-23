3e8c1703b6f89b11d584681c92a314a74ad73346
Guillaume <guillaume.trb@hotmail.com>
2021-07-23 12:00:48 +0200

    fix: changes sensor types names and units

--- src/DataFixtures/SensorTypeFixtures.php
+++ src/DataFixtures/SensorTypeFixtures.php
@@ -19,18 +19,17 @@ class SensorTypeFixtures extends Fixture
         $sensorTypes = [
             ['name' => 'Température de l\'air', 'unit' => '°C', 'influxId' => 'Air temperature'],
             ['name' => 'Température de l\'eau', 'unit' => '°C', 'influxId' => 'Water temperature'],
-            ['name' => 'Hygrométrie', 'unit' => '%', 'influxId' => 'EC'],
-            ['name' => 'pH', 'unit' => '%', 'influxId' => 'PH'],
-            ['name' => 'Luminosité', 'unit' => '', 'influxId' => 'Luminosity'],
-            ['name' => 'Humidité', 'unit' => '', 'influxId' => 'Humidity'],
+            ['name' => 'Qualité de l\'eau', 'unit' => 'mS/cm', 'influxId' => 'EC'],
+            ['name' => 'pH', 'unit' => 'pH', 'influxId' => 'PH'],
+            ['name' => 'Luminosité', 'unit' => 'lux', 'influxId' => 'Luminosity'],
+            ['name' => 'Humidité', 'unit' => '%', 'influxId' => 'Humidity'],
         ];
 
-        foreach ($sensorTypes as $sensorType){
+        foreach ($sensorTypes as $sensorType) {
             $s = (new SensorType())
                 ->setName($sensorType['name'])
                 ->setUnit($sensorType['unit'])
-                ->setInflexId($sensorType['influxId'])
-            ;
+                ->setInflexId($sensorType['influxId']);
 
             $manager->persist($s);
         }
