5026acc357db5fe76ce28cef340337a206139d2b
Guillaume <guillaume.trb@hotmail.com>
2021-07-26 11:07:59 +0200

    feat: adds sensor type icon (images, fixtures and migration)

--- src/DataFixtures/SensorTypeFixtures.php
+++ src/DataFixtures/SensorTypeFixtures.php
@@ -17,19 +17,20 @@ class SensorTypeFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $sensorTypes = [
-            ['name' => 'Température de l\'air', 'unit' => '°C', 'influxId' => 'Air temperature'],
-            ['name' => 'Température de l\'eau', 'unit' => '°C', 'influxId' => 'Water temperature'],
-            ['name' => 'Qualité de l\'eau', 'unit' => 'mS/cm', 'influxId' => 'EC'],
-            ['name' => 'pH', 'unit' => 'pH', 'influxId' => 'PH'],
-            ['name' => 'Luminosité', 'unit' => 'lux', 'influxId' => 'Luminosity'],
-            ['name' => 'Humidité', 'unit' => '%', 'influxId' => 'Humidity'],
+            ['name' => 'Température de l\'air', 'unit' => '°C', 'influxId' => 'Air temperature', 'icon_path' => '/images/sensor_icons/Temperatur_dair.svg'],
+            ['name' => 'Température de l\'eau', 'unit' => '°C', 'influxId' => 'Water temperature', 'icon_path' => '/images/sensor_icons/Temperatur_deau.svg'],
+            ['name' => 'Qualité de l\'eau', 'unit' => 'mS/cm', 'influxId' => 'EC', 'icon_path' => '/images/sensor_icons/Nurriture.svg'],
+            ['name' => 'pH', 'unit' => 'pH', 'influxId' => 'PH', 'icon_path' => '/images/sensor_icons/Ph.svg'],
+            ['name' => 'Luminosité', 'unit' => 'lux', 'influxId' => 'Luminosity', 'icon_path' => '/images/sensor_icons/Lumiere.svg'],
+            ['name' => 'Humidité', 'unit' => '%', 'influxId' => 'Humidity', 'icon_path' => '/images/sensor_icons/Huminite_Air.svg'],
         ];
 
         foreach ($sensorTypes as $sensorType) {
             $s = (new SensorType())
                 ->setName($sensorType['name'])
                 ->setUnit($sensorType['unit'])
-                ->setInflexId($sensorType['influxId']);
+                ->setInflexId($sensorType['influxId'])
+                ->setIconPath($sensorType['icon_path']);
 
             $manager->persist($s);
         }
