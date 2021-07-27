5eded13d7c72c50e49b5a87ef6c0abdedf311b5c
Guillaume <guillaume.trb@hotmail.com>
2021-07-27 20:46:49 +0200

    feat: creates some "real" comments in the fixtures

--- src/DataFixtures/CommentFixtures.php
+++ src/DataFixtures/CommentFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Comment;
+use App\Entity\Image;
 use App\Entity\Module;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -13,18 +14,14 @@ class CommentFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        $faker = Factory::create();
-
         $modules = $manager->getRepository(Module::class)->findAll();
 
-        for ($i = 0; $i < 4; $i++) {
-            $comment = (new Comment())
-                ->setText('Ceci est un commentaire.')
-                ->setModule($faker->randomElement($modules))
-            ;
-
-            $manager->persist($comment);
-        }
+        $this->createAndPersistFakeComment($manager, $modules[0], 'La première fraise mûre !!', 'https://res.cloudinary.com/hl4a3z5co/image/upload/v1627408346/dev/comments/hcmtte5ycidnzsaypaqe.jpg');
+        $this->createAndPersistFakeComment($manager, $modules[0], 'Les racines des jeunes pousses ont bien poussé', 'https://res.cloudinary.com/hl4a3z5co/image/upload/v1627408348/dev/comments/gqjzypsvjfdwrjvfn5cc.jpg');
+        $this->createAndPersistFakeComment($manager, $modules[0], 'Madame Perez qui pose à côté des plantes avant la récolte', 'https://res.cloudinary.com/hl4a3z5co/image/upload/v1627408349/dev/comments/iq6u5k9uzv0xmszm54ta.jpg');
+        $this->createAndPersistFakeComment($manager, $modules[0], 'Aujourd\'hui on a planté de nouveaux semis. Il y a des tomates, des fraises et des concombres.');
+        $this->createAndPersistFakeComment($manager, $modules[1], 'Aujourd\'hui on a renouvelé l\'eau des bacs et on a vu pourquoi c\'est important pour les plantes d\'avoir une eau de bonne qualité');
+        $this->createAndPersistFakeComment($manager, $modules[1], 'Les laitues après 3 semaines', 'https://res.cloudinary.com/hl4a3z5co/image/upload/v1627408347/dev/comments/o7wuy5fhvqohkabvnhuz.jpg');
 
         $manager->flush();
     }
@@ -34,5 +31,28 @@ class CommentFixtures extends Fixture implements DependentFixtureInterface
         return [
             ModuleFixtures::class
         ];
+    }
+
+    private function createAndPersistFakeComment(ObjectManager $manager, Module $module, string $text, string $imageUrl = null): void
+    {
+        $faker = Factory::create();
+
+        $comment = (new Comment())
+            ->setText($text)
+            ->setModule($module);
+
+        if ($imageUrl) {
+            $image = (new Image())
+                ->setComment($comment)
+                ->setCreatedAt(new \DateTimeImmutable($faker->dateTimeThisMonth()->format('Y-m-d H:i:s')))
+                ->setFormat('jpg')
+                ->setPublicId('blabla')
+                ->setSecureUrl($imageUrl);
+
+            $manager->persist($image);
+            $comment->addImage($image);
+        }
+
+        $manager->persist($comment);
     }
 }
